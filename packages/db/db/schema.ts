6c32460bafbc2544d10e437689b0e7ebd9e99e8a
infoanilsahu <info.anilsahu@gmail.com>
2026-08-06 15:29:16 +0530

    time set default not null

--- packages/db/db/schema.ts
+++ packages/db/db/schema.ts
@@ -6,7 +6,7 @@ export const account = pgTable("Account", {
     id: uuid().primaryKey(),
     email: varchar().notNull(),
     provider: Providers(),
-    createdAt: timestamp('created_at').defaultNow(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
     updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
 
@@ -14,6 +14,6 @@ export const org = pgTable("Org", {
     id: uuid().primaryKey(),
     name: varchar().notNull(),
     accountId: uuid().notNull().references(() => account.id),
-    createdAt: timestamp('created_at').defaultNow(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
     updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
