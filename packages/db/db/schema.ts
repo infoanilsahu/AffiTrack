3eec1545dc050e02f639d0026e4c9b9dd1cb3500
infoanilsahu <info.anilsahu@gmail.com>
2026-08-08 21:29:53 +0530

    fix(schema): update account and org schema

--- packages/db/db/schema.ts
+++ packages/db/db/schema.ts
@@ -1,19 +1,21 @@
-import { pgTable, integer, uuid, varchar, pgEnum, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, uuid, varchar, pgEnum, timestamp } from "drizzle-orm/pg-core";
 
-export const Providers = pgEnum("provider",["google", "email"])
+export const Providers = pgEnum("provider", ["google", "email"]);
 
-export const account = pgTable("Account", {
-    id: uuid().defaultRandom().primaryKey(),
-    email: varchar().notNull().unique(),
-    provider: Providers().notNull(),
-    createdAt: timestamp('created_at').defaultNow().notNull(),
-    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+export const account = pgTable("account", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    email: varchar("email", { length: 255 }).notNull().unique(),
+    provider: Providers("provider").notNull(),
+    createdAt: timestamp("created_at", { withTimezone: false }).defaultNow().notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: false }).defaultNow().notNull(),
 });
 
-export const org = pgTable("Org", {
-    id: uuid().defaultRandom().primaryKey(),
-    name: varchar().notNull(),
-    accountId: uuid().notNull().references(() => account.id),
-    createdAt: timestamp('created_at').defaultNow().notNull(),
-    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+export const org = pgTable("org", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    name: varchar("name", { length: 255 }).notNull(),
+    accountId: uuid("account_id")
+        .notNull()
+        .references(() => account.id, { onDelete: "cascade" }),
+    createdAt: timestamp("created_at", { withTimezone: false }).defaultNow().notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: false }).defaultNow().notNull(),
 });
